be69c8ce18ad4d9803d008c797030e2194c6a2dc
Rishabh Agrawal <r.agrawal@dyninno.in>
2025-05-30 17:43:48 +0530

    Update header content in App component to reflect user name and course details

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ function App() {
 
   return (
     <>
-      <h1>Github Actions</h1>
+      <h1>Rishabh Agrawal</h1>
       <h2>Complete course with 2 projects</h2>
+      <h3>React + Vite</h3>
     </>
   )
 }
